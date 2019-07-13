4532364a5e4aba290b25faed1c4b5d62bcf75d46
Sevada Saakyan <sevada992@gmail.com>
2019-07-13 20:32:20 +0300

    The expandable list view added

--- app/src/main/java/com/flex/market/CatalogFragment.java
+++ app/src/main/java/com/flex/market/CatalogFragment.java
@@ -7,11 +7,68 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class CatalogFragment extends Fragment {
+    private ExpandableListView listView;
+    private ExpandableListAdapter listAdapter;
+    private List<String> listCategories;
+    private HashMap<String, List<String>> listHashMap;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.catalog_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        listView = view.findViewById(R.id.listViewCategories);
+        listView.setGroupIndicator(null);
+
+        // Fetch data here
+        listCategories = new ArrayList<>();
+        listHashMap = new HashMap<>();
+
+        listCategories.add("Кровельные элементы");
+        listCategories.add("Доборные элементы");
+        listHashMap.put("Кровельные элементы", new ArrayList<String>(){{add("Крыша 1"); add("Крыша 2");}});
+        listHashMap.put("Доборные элементы", new ArrayList<String>(){{add("Кирпич 1"); add("Кирпичик 2"); add("Камень 1");}});
+
+        listAdapter = new ExpandableListAdapter(getContext(), listCategories, listHashMap);
+        listView.setAdapter(listAdapter);
+
+        listView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                if (parent.isGroupExpanded(groupPosition)) {
+                    if (ExpandableListAdapter.selectedItemId == groupPosition) {
+                        ExpandableListAdapter.selectedItemId = -1;
+                        listAdapter.notifyDataSetChanged();
+                    }
+                } else {
+                    ExpandableListAdapter.selectedItemId = groupPosition;
+                    listAdapter.notifyDataSetChanged();
+                }
+
+                return false;
+            }
+        });
+        listView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            int lastExpandedPosition = -1;
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                if (groupPosition != lastExpandedPosition) {
+                    listView.collapseGroup(lastExpandedPosition);
+                }
+                lastExpandedPosition = groupPosition;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/flex/market/ExpandableListAdapter.java
@@ -0,0 +1,100 @@
+package com.flex.market;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+    private Context context;
+    private List<String> listCategories;
+    private HashMap<String, List<String>> listHashMap;
+    static int selectedItemId = -1;
+
+    ExpandableListAdapter(Context context, List<String> listCategories, HashMap<String, List<String>> listHashMap) {
+        this.context = context;
+        this.listCategories = listCategories;
+        this.listHashMap = listHashMap;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return listCategories.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return listHashMap.get(listCategories.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return listCategories.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return listHashMap.get(listCategories.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        String category = (String)getGroup(groupPosition);
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_category, null);
+        }
+
+        TextView listCategory = convertView.findViewById(R.id.listCategory);
+        listCategory.setText(category);
+        listCategory.setTextColor(parent.getContext().getResources().getColor(R.color.gray));
+
+        if (selectedItemId == groupPosition) {
+            listCategory.setTextColor(parent.getContext().getResources().getColor(R.color.colorPrimary));
+        }
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        String child = (String)getChild(groupPosition, childPosition);
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_category_item, null);
+        }
+
+        TextView listChild = convertView.findViewById(R.id.listItem);
+        listChild.setText(child);
+        listChild.setTextColor(parent.getContext().getResources().getColor(R.color.gray));
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
