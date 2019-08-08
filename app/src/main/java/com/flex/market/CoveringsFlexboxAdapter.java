cdf3e120b4d320980b0628be11346cd9b0b8aa10
Sevada Saakyan <sevada992@gmail.com>
2019-08-08 15:53:36 +0300

    Covering selection implemented

--- app/src/main/java/com/flex/market/CoveringsFlexboxAdapter.java
+++ app/src/main/java/com/flex/market/CoveringsFlexboxAdapter.java
@@ -7,12 +7,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class CoveringsFlexboxAdapter extends RecyclerView.Adapter<CoveringsFlexboxAdapter.CoveringViewHolder> {
     static ArrayList<String> coverings = new ArrayList<>();
     static int selectedCovering = -1;
+    private static CoveringViewHolder previousSelectedViewHolder;
 
     @NonNull
     @Override
@@ -22,7 +24,28 @@ public class CoveringsFlexboxAdapter extends RecyclerView.Adapter<CoveringsFlexb
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CoveringViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull final CoveringViewHolder viewHolder, int i) {
+        viewHolder.layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (previousSelectedViewHolder != null) {
+                    previousSelectedViewHolder.layout.setCardBackgroundColor(
+                            v.getResources().getColor(android.R.color.white)
+                    );
+                }
+
+                selectedCovering = viewHolder.getAdapterPosition();
+
+                Toast.makeText(v.getContext(), coverings.get(selectedCovering), Toast.LENGTH_LONG).show();
+
+                viewHolder.layout.setCardBackgroundColor(
+                        v.getResources().getColor(R.color.colorControlHighlight)
+                );
+
+                previousSelectedViewHolder = viewHolder;
+            }
+        });
+
         viewHolder.name.setText(coverings.get(i));
     }
 
